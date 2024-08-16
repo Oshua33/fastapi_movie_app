80d2b568e3b8b47eb036b0f41631320121017669
oshua33 <oshuaaigbor@gmail.com>
2024-08-16 02:26:41 -0700

    test update

--- app/test/test_app.py
+++ app/test/test_app.py
@@ -64,22 +64,7 @@ def test_login(setup_db):
     assert "access_token" in response.json()
 
 
-# Test login with wrong credentials
-# @pytest.mark.parametrize("username, password, status_code", [
-#     ("testwronguser", "testpassword", status.HTTP_401_UNAUTHORIZED),
-#     ("testuser", "wrongpassword", status.HTTP_401_UNAUTHORIZED),
-#     ("testwronguser", "wrongpassword", status.HTTP_401_UNAUTHORIZED),
-#     (None, "testpassword", status.HTTP_422_UNPROCESSABLE_ENTITY),
-#     ("testuser", None, status.HTTP_422_UNPROCESSABLE_ENTITY)
-# ])
-# def test_wrong_login(setup_db, username, password, status_code):
-#     response = client.post("/login", data={
-#         "username": username or "",
-#         "password": password or ""
-#     })
-#     assert response.status_code == status_code
-
-
+# @pytest.fixture
 def test_create_movie(setup_db):
     login_response = client.post("/login", data={
         "username": "testuser",
@@ -87,7 +72,7 @@ def test_create_movie(setup_db):
     })
     token = login_response.json()["access_token"]
 
-    response = client.post("/movies/", json={
+    movie_response = client.post("/movies/", json={
         "title": "Test Movie",
         "genre": "A test movie description",
         "publisher": "Test Publisher",
@@ -95,9 +80,13 @@ def test_create_movie(setup_db):
     }, headers={
         "Authorization": f"Bearer {token}"
     })
-    print(response.json())  # Debugging line to print the error response
-    assert response.status_code == 201
-    assert response.json()["title"] == "Test Movie"
+    print(movie_response.json())  # Debugging line to print the error response
+    assert movie_response.status_code == 201
+    # assert response.json()["title"] == "Test Movie"
+    movie_data = movie_response.json()
+    
+    return movie_data, token
+
 
 def test_get_movies(setup_db):
     response = client.get("/movies/")
@@ -159,6 +148,113 @@ def test_delete_movie(setup_db):
         # assert response.json()["id"] == '1'
         assert response.json()['message'] == f'movie {movie_id} deleted successfully'
 
+  
+  
+  
+    # rating test......
     
-        
+    # create rating
+def test_create_rating(setup_db):
+    login_response = client.post("/login", data={
+    "username": "testuser",
+    "password": "testpassword"
+    })
+    token = login_response.json()["access_token"]
+     
+    # Create a movie before creating the rating
+    # suppose be fixture oo, movie data
+    movie_response = client.post("/movies", json={
+            "title": "Test Movie",
+            "genre": "A test movie description",
+            "publisher": "Test Publisher",
+            "year_published": "2024"
+    }, headers={
+        "Authorization": f"Bearer {token}"
+    })
+    
+    # Ensure the movie was created 
+    assert movie_response.status_code == 201
+    movie_id = movie_response.json()['id']
+    
+    response = client.post(f'/ratings/{movie_id}', json={"rating": "4.0"}, headers={
+        "Authorization": f"Bearer {token}"
+    })
+    # assert it rating created successfully
+    assert response.status_code == 200
+    assert response.json()['rating'] == 4.0
+
+
+# def test_create_rating(setup_db, create_movie):
+#     movie_data, token = create_movie
+    
+#     response = client.post(f'/ratings/{movie_data["id"]}', json={"rating": "4.0"})
+
+#     # Retrieve ratings for the movie
+#     response = client.get(f"/ratings/{movie_data['id']}")
+
+
+#     assert response.status_code == 200
+#     assert len(response.json()) == 0
+#     assert response.json()[0]['movie_id'] == movie_data['id']
+
+
+    # create rating for movie not found
+def test_create_rating_wrond_movie_id(setup_db):
+    login_response = client.post("/login", data={
+    "username": "testuser",
+    "password": "testpassword"
+    })
+    token = login_response.json()["access_token"]
+    
+    wrong_movie_id = 4444
+    
+    response = client.post('/ratings/{wrong_movie_id}', json={"rating": "4.0"}, headers={
+        "Authorization": f"Bearer {token}"
+    })
+    
+    assert response.status_code == 404
+    assert response.json()['detail'] == 'movie not found'
+    
+    
+# # ratings for a movie
+# def test_get_rating_by_movie(setup_db):
+#     movie_id = client.post(f"/movies{movie_id}", json={
+#             "title": "Test Movie",
+#             "genre": "A test movie description",
+#             "publisher": "Test Publisher",
+#             "year_published": "2024"
+#     })
+#     assert movie_id.status_code == 201
+#     movie_id = movie_id.json()['id']
+    
+#     response = client.get(f"/ratings/{movie_id}/rate")
+    
+#     assert response.status_code == 200
+#     assert response.json()['id'] == movie_id
+    
+
+    
+
+# create rating
+# def test_create_rating(setup_db):
+#     login_response = client.post("/login", data={
+#     "username": "testuser",
+#     "password": "testpassword"
+#     })
+#     token = login_response.json()["access_token"]
+     
+#     # Create a movie before creating the rating
+#     # suppose be fixture oo, movie data
+   
+    
+#     # Ensure the movie was created 
+    
+    
+#     response = client.post(f'/ratings/{movie_id}', json={"rating": "4.0"}, headers={
+#         "Authorization": f"Bearer {token}"
+#     })
+#     # assert it rating created successfully
+#     assert response.status_code == 200
+#     assert response.json()['rating'] == 4.0
+
 
